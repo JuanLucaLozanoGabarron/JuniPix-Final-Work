7283d7c7e12cd48cdbb3c22d32832e182ca8365d
JuanLucaLozanoGabarron <juan-luca.lozano.gabarron@student.ehb.be>
2024-05-02 14:00:39 +0200

    Homepage WIP

--- junipix/src/routes/Homepage.jsx
+++ junipix/src/routes/Homepage.jsx
@@ -1,13 +1,21 @@
 import "./style/App.css";
 import Header from "../components/Header";
-import Collage from "../components/Collage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="firstInfo">
-        <Collage />
+        <div className="text">
+          <div className="title">
+            <h2>WELCOME TO</h2>
+            <h1>JUNIPIX</h1>
+          </div>
+          <div className="createYourArtpiece">
+            <input type="text" placeholder="Create your own artpiece" />
+          </div>
+        </div>
+        <div className="collage"></div>
       </div>
     </div>
   );
